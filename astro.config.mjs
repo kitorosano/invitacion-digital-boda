ede8f3a751342f8eb5bbf9800a56b39656b58d92
Kitorosano <rosanoesteban2@gmail.com>
2025-09-16 23:29:16 -0300

    feat: :sparkles: added hero component, with env values
    also implemented bgm music on play

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  env: {
+    schema: {
+      WEDDING_COUPLE: envField.string({
+        default: "",
+        context: "server",
+        access: "public",
+      }),
+      HERO_TITLE: envField.string({
+        default: "",
+        context: "server",
+        access: "public",
+      }),
+      WEDDING_DATE: envField.string({
+        default: "",
+        context: "server",
+        access: "public",
+      }),
+    },
+  },
+
+  adapter: vercel(),
+});
