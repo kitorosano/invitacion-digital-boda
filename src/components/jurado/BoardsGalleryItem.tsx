da0f1fecff19a927c88240f132732f1bce52d6b5
Kitorosano <rosanoesteban2@gmail.com>
2025-11-10 18:22:08 -0300

    style: :lipstick: user board title

--- src/components/jurado/BoardsGalleryItem.tsx
+++ src/components/jurado/BoardsGalleryItem.tsx
@@ -14,8 +14,8 @@ const BoardsGalleryItem = ({ user, position, onBoardClick }: Props) => {
 
   return (
     <li className="boards-gallery-item-container" onClick={handleBoardClick}>
-      <span className="position-indicator">{position + 1}. </span>
-      {user.name}
+      <span>{position + 1}. </span>
+      <p >{user.name}</p>
     </li>
   );
 };
